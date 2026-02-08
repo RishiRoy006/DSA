75f29f3be7902f430da9aa50c30ad47cb08327ed
RishiRoy006 <117927928+RishiRoy006@users.noreply.github.com>
2026-02-09 01:25:04 +0530

    Time: 8 ms (86.82%) | Memory: 46 MB (83.02%) - LeetSync

---
+++ 326-power-of-three/power-of-three.java
@@ -0,0 +1,11 @@
+class Solution {
+    public boolean isPowerOfThree(int n) {
+        if (n <= 0) return false;
+
+        long val = 1;
+        while (val < n) {
+            val *= 3;
+        }
+        return val == n;
+    }
+}
